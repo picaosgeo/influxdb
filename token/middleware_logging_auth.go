095d3cc92cacc4d38b71f1e693baf12a88750f62
Alirie Gray <alirie.gray@gmail.com>
2020-04-21 21:01:43 -0700

    add logging functions

--- token/middleware_logging_auth.go
+++ token/middleware_logging_auth.go
@@ -1,6 +1,10 @@
 package token
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/influxdata/influxdb/v2"
 	"go.uber.org/zap"
 )
@@ -16,4 +20,56 @@ func NewAuthLogger(log *zap.Logger, s influxdb.AuthorizationService) *AuthLogger
 		logger:      log,
 		authService: s,
 	}
+}
+
+var _ influxdb.AuthorizationService = (*AuthLogger)(nil)
+
+func (l *AuthLogger) CreateAuthorization(ctx context.Context, a *influxdb.Authorization) (err error) {
+	defer func(start time.Time) {
+		dur := zap.Duration("took", time.Since(start))
+		if err != nil {
+			l.logger.Error("failed to create authorization", zap.Error(err), dur)
+			return
+		}
+		l.logger.Debug("authorization create", dur)
+	}(time.Now())
+	return l.authService.CreateAuthorization(ctx, a)
+}
+
+func (l *AuthLogger) FindAuthorizationByID(ctx context.Context, id influxdb.ID) (a *influxdb.Authorization, err error) {
+	defer func(start time.Time) {
+		dur := zap.Duration("took", time.Since(start))
+		if err != nil {
+			msg := fmt.Sprintf("failed to find authorization with ID %v", id)
+			l.logger.Error(msg, zap.Error(err), dur)
+			return
+		}
+		l.logger.Debug("auth find by ID", dur)
+	}(time.Now())
+	return l.authService.FindAuthorizationByID(ctx, id)
+}
+
+func (l *AuthLogger) FindAuthorization(ctx context.Context, t string) (a *influxdb.Authorization, err error) {
+	defer func(start time.Time) {
+		dur := zap.Duration("took", time.Since(start))
+		if err != nil {
+			l.logger.Error("failed to find authorization mwith token", zap.Error(err), dur)
+			return
+		}
+		l.logger.Debug("auth find", dur)
+
+	}(time.Now())
+	return l.authService.FindAuthorizationByToken(ctx, t)
+}
+
+func (l *AuthLogger) FindAuthorizations(ctx context.Context, filter influxdb.AuthorizationFilter, opt ...FindOptions) ([]*Authorization, int, error) {
+	return nil, 0, nil
+}
+
+func (l *AuthLogger) UpdateAuthorization(ctx context.Context, id influxdb.ID, udp *influxdb.AuthorizationUpdate) (*Authorization, error) {
+	return nil, nil
+}
+
+func (l *AuthLogger) DeleteAuthorization(ctx context.Context, id influxdb.ID) error {
+	return nil
 }
